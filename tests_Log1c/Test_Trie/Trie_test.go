858c81535422a9f90d2da6de3ad9285f1342794d
Log1c <add398sfj@gmail.com>
2022-10-23 01:17:55 +0800

    Update Trie_test.go

--- tests_Log1c/Test_Trie/Trie_test.go
+++ tests_Log1c/Test_Trie/Trie_test.go
@@ -8,8 +8,73 @@
 
 package Test_Trie
 
-import "testing"
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	"github.com/ethereum/go-ethereum/trie"
+	"math/rand"
+	"testing"
+)
 
 func Test_Trie_Store(t *testing.T) {
-	t :=
+	diskdb := memorydb.New()
+	triedb := trie.NewDatabase(diskdb)
+	random := rand.New(rand.NewSource(0))
+
+	size := 10000000
+	keys := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		k := make([]byte, 20)
+		random.Read(k)
+		keys[i] = k
+	}
+
+	fmt.Println(1)
+
+	tree := trie.NewEmpty(triedb)
+	for i := 0; i < size; i++ {
+
+		tree.Update(keys[i], []byte("1"))
+	}
+
+	fmt.Println(2)
+
+	fmt.Println(3)
+
+
+
 }
+
+func Benchmark_Test_Trie(b *testing.B) {
+	diskdb := memorydb.New()
+	triedb := trie.NewDatabase(diskdb)
+	random := rand.New(rand.NewSource(0))
+
+	size := 20000000
+	keys := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		k := make([]byte, 20)
+		random.Read(k)
+		keys[i] = k
+	}
+
+	tree := trie.NewEmpty(triedb)
+	for i := 0; i < size; i++ {
+
+		tree.Update(keys[i], []byte("1"))
+	}
+
+	b.ResetTimer()
+	for j := 0; j < b.N; j++ {
+		for i := 0; i < size; i++ {
+			if i % 2000 == 0 {
+				tree.TryGet(keys[i])
+			}
+			//val, _ := tree.TryGet(keys[i])
+			//fmt.Println(keys[i])
+			//fmt.Println(val)
+		}
+	}
+	b.StopTimer()
+}
+
