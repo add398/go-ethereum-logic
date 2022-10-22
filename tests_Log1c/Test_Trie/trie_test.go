580c72b2cf9378a5a63a04d2f7f7a7161fd82ac6
Log1c <add398sfj@gmail.com>
2022-10-22 20:36:10 +0800

    update name

---
+++ tests_Log1c/Test_Trie/trie_test.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: Log1c
+ * @Description:
+ * @File:  trie_test
+ * @Version: 1.0.0
+ * @Date: 2022/10/22 15:28
+ */
+
+package Test_Trie
+
+import "testing"
+
+func Test_Trie_Store(t *testing.T) {
+
+}
