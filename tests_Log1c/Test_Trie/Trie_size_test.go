af8d3e5bcf4d85e0d8fb22a067a1a618f053d829
“Log1c <37044295+add398@users.noreply.github.com>
2022-12-03 16:37:03 +0800

    update gitignore

--- tests_Log1c/Test_Trie/Trie_size_test.go
+++ tests_Log1c/Test_Trie/Trie_size_test.go
@@ -8,7 +8,46 @@
 
 package Test_Trie
 
-import "testing"
+import (
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	"github.com/ethereum/go-ethereum/trie"
+	"math/rand"
+	"testing"
+)
+
+func Test_random_key_value_space(t *testing.T) {
+	diskdb := memorydb.New()
+	triedb := trie.NewDatabase(diskdb)
+	random := rand.New(rand.NewSource(0))
+
+	keys := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		k := make([]byte, 20)
+		random.Read(k)
+		keys[i] = k
+	}
+
+	value := make([]byte, 0)
+	for i := 0; i < 5; i++ {
+		value = append(value, keys[i]...)
+	}
+	//fmt.Println(value)
+
+	tree := trie.NewEmpty(triedb)
+	for i := 0; i < size; i++ {
+		tree.Update(keys[i], []byte(""))
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 func Benchmark_read_from_trie_100w(b *testing.B) {
 	size := 1000000
