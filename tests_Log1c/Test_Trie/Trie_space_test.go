377465e815154a3e2aa23656b40319a778c5daaf
“Log1c <37044295+add398@users.noreply.github.com>
2022-12-04 00:16:04 +0800

    Update Trie_space_test.go

--- tests_Log1c/Test_Trie/Trie_space_test.go
+++ tests_Log1c/Test_Trie/Trie_space_test.go
@@ -10,7 +10,6 @@ package Test_Trie
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/ethdb/memorydb"
 	"github.com/ethereum/go-ethereum/trie"
 	"math/rand"
@@ -20,7 +19,7 @@ import (
 
 
 func Benchmark_test_space(b *testing.B) {
-	size := 1000000
+	size := 100000
 	acc, val := makeAccounts(size)
 	for i := 0; i < b.N; i++ {
 		benchmark_space(b, acc, val)
@@ -35,13 +34,16 @@ func benchmark_space(b *testing.B, addresses [][]byte, value []byte)  {
 	b.ReportAllocs()
 	b.StartTimer()
 
-	trie := trie.NewEmpty(trie.NewDatabase(rawdb.NewMemoryDatabase()))
+	trie := trie.NewEmpty(trie.NewDatabase(memorydb.New()))
 	for i := 0; i < len(addresses); i++ {
+		if i % 100 == 0 {
+			fmt.Println(i )
+		}
 		trie.Update(addresses[i], value)
 	}
 	//v := trie.Get(addresses[0])
 	//fmt.Println(v)
-
+	//trie.Commit(false)
 
 	// commit 以后不可以 get数据
 	//v = trie.Get(addresses[0])
@@ -66,7 +68,7 @@ func makeAccounts(size int) (addresses [][]byte, value []byte) {
 		addresses[i] = k
 	}
 
-	value = make([]byte, 100)
+	value = make([]byte, 10)
 	random.Read(value)
 	return
 }
