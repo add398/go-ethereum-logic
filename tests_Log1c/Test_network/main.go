82db82f1e3deea6335682d94d78cf0175f0db75a
Log1c <add398sfj@gmail.com>
2023-01-18 19:34:06 +0800

    network

---
+++ tests_Log1c/Test_network/main.go
@@ -0,0 +1,71 @@
+/**
+ * @Author: Log1c
+ * @Description:
+ * @File:  main
+ * @Version: 1.0.0
+ * @Date: 2023/1/18 18:57
+ */
+
+package main
+
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+
+func makeAccounts(size int) (addresses [][]byte, value []byte) {
+	// Make the random benchmark deterministic
+	random := rand.New(rand.NewSource(0))
+	addresses = make([][]byte, size)
+	for i := 0; i < size; i++ {
+		k := make([]byte, 20)
+		random.Read(k)
+		addresses[i] = k
+	}
+
+	value = make([]byte, 100)
+	random.Read(value)
+	return
+}
+
+
+
+
+
+func main() {
+	size := 10000
+	//keys, value := makeAccounts(size)
+	url := "192.168.18.101:8080/ping"
+
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	start := time.Now() // 获取当前时间
+
+	for i := 0; i < size; i++ {
+		resp, _ = http.Get(url)
+
+	}
+	defer resp.Body.Close()
+	elapsed1 := time.Since(start)
+	fmt.Println("该函数执行完成耗时：", elapsed1.Milliseconds())
+
+
+	ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("ok")
+	}
+
+
+
+}
